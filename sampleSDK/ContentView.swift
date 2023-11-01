fc5a414e17b941acf794bb7e1be6620af8115aec
JonnavithulaGirish <girish@transcend.io>
2023-11-01 11:36:36 -0500

    Initial Commit

---
+++ sampleSDK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleSDK
+//
+//  Created by Girish Jonnavithula on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleSDK/sampleSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleSDKApp.swift
+//  sampleSDK
+//
+//  Created by Girish Jonnavithula on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
