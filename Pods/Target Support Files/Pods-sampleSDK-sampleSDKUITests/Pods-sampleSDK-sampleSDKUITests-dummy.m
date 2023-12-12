03fc9f7f200ac3325fd3ca93fa25fbe722b91d18
JonnavithulaGirish <girish@transcend.io>
2023-12-11 20:05:56 -0600

    Use TranscendAPI

--- Pods/Target Support Files/Pods-sampleSDK-sampleSDKUITests/Pods-sampleSDK-sampleSDKUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sampleSDK_sampleSDKUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sampleSDK_sampleSDKUITests
-@end
--- sampleSDK/sampleSDKApp.swift
+++ sampleSDK/sampleSDKApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
-
+import Transcend
 @main
 struct sampleSDKApp: App {
+    @State var canUseAPI: Bool = false
+    
     var body: some Scene {
+        let didFinishNavigation: ((Result<Void,Error>) -> Void) = { result in
+            switch result {
+            case .success():
+                self.canUseAPI = true
+            case .failure(let error):
+                print("Error during web view navigation: \(error.localizedDescription)")
+            }
+        }
+        
         WindowGroup {
-            ContentView()
+            ContentView(canUseAPI: $canUseAPI)
+            TranscendWebViewUI(transcendConsentUrl: "https://transcend-cdn.com/cm/a3b53de6-5a46-427a-8fa4-077e4c015f93/airgap.js",
+                                    isInit: true, didFinishNavigation: didFinishNavigation)
         }
     }
 }
