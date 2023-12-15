bff2d1a13abaf7b2892031b2a8a5f5b468dc6afa
JonnavithulaGirish <girish@transcend.io>
2023-12-15 13:44:49 -0600

    Use onCloseListener

--- sampleSDK/sampleSDKApp.swift
+++ sampleSDK/sampleSDKApp.swift
@@ -21,8 +21,6 @@ struct sampleSDKApp: App {
             }
         }
         
-        
-        
         WindowGroup {
             ContentView(canUseAPI: $canUseAPI)
             TranscendWebViewUI(transcendConsentUrl: "https://transcend-cdn.com/cm/a3b53de6-5a46-427a-8fa4-077e4c015f93/airgap.js",
