93b8c4cb3aa54473aa8b046604c54dc1d913ac73
JonnavithulaGirish <girish@transcend.io>
2024-01-12 13:00:55 -0500

    Initial Commit

---
+++ sampleSDK/sampleSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleSDKApp.swift
+//  sampleSDK
+//
+//  Created by Girish Jonnavithula on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
