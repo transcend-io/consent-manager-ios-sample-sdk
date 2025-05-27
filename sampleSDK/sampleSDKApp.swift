a39e341e16390e3ddd1c2b13a62deab601b2d912
JonnavithulaGirish <girish@transcend.io>
2025-05-27 15:44:15 -0400

    Use 1.2.0

--- sampleSDK/sampleSDKApp.swift
+++ sampleSDK/sampleSDKApp.swift
@@ -22,19 +22,16 @@ struct sampleSDKApp: App {
             }
         }
         
-        // Simple config object
-        // expects only your airgap bundle url
-        let transcendCoreConfigSimple: TranscendCoreConfig = TranscendCoreConfig(transcendConsentUrl: "https://transcend-cdn.com/cm-test/c7561f1c-7ec9-498c-a401-7219e3b36a8c/airgap.js")
         
         // If you have any webView that you would open on this application
         // and need to carry forward the consent state the use syncDomain
         // In the line below, we have include sync domain "https://example.com/"
         // Which might, at some point, in this application be opened as a webview
         // and you might require consent data to passed on to this webview
-        let transcendCoreConfigWithSyncDomain: TranscendCoreConfig = TranscendCoreConfig(transcendConsentUrl: "https://transcend-cdn.com/cm-test/c7561f1c-7ec9-498c-a401-7219e3b36a8c/airgap.js", syncDomains: ["https://example.com/"], mobileAppId: "com.transcend.ios")
+        let transcendCoreConfigWithSyncDomain: TranscendCoreConfig = TranscendCoreConfig(transcendConsentUrl: "https://transcend-cdn.com/cm-test/c45a9cb2-27be-4528-b25a-8696a3ee6852/airgap.js", syncDomains: ["https://example.com/"], mobileAppId: "iOS")
         
         // If you want to use preference store to sync logged In users data
-        let transcendCoreConfigWithPrefSync: TranscendCoreConfig = TranscendCoreConfig(transcendConsentUrl: "https://transcend-cdn.com/cm-test/c7561f1c-7ec9-498c-a401-7219e3b36a8c/airgap.js", token: "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWRJZGVudGlmaWVyIjoiK3dJWXk2SkdmcGxaUUZMWS9ETnQrTUNRS0dISENWckYiLCJpYXQiOjE3MDY5MTA2ODd9.d4zZoMPtriAPwC0HvJ6BqkOGdG_qcPjmRYNNkN_MfLvZDob1OzQcFUbfKFtFZKix")
+        let transcendCoreConfigWithPrefSync: TranscendCoreConfig = TranscendCoreConfig(transcendConsentUrl: "https://transcend-cdn.com/cm-test/c45a9cb2-27be-4528-b25a-8696a3ee6852/airgap.js", token: "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWRJZGVudGlmaWVyIjoiK3dJWXk2SkdmcGxaUUZMWS9ETnQrTUNRS0dISENWckYiLCJpYXQiOjE3MDY5MTA2ODd9.d4zZoMPtriAPwC0HvJ6BqkOGdG_qcPjmRYNNkN_MfLvZDob1OzQcFUbfKFtFZKix", mobileAppId: "iOS")
         
         WindowGroup {
             ContentView(canUseAPI: $canUseAPI)
